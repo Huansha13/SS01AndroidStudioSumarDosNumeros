9c34d0a7381dc3fbc5eb6240a8623c78702fab30
Huansha13 <ing.yfhl13@outlook.com>
2023-03-08 22:33:32 -0500

    sumar dos numeros

---
+++ app/src/main/java/xyz/android/sumardosnumeros/MainActivity.java
@@ -0,0 +1,38 @@
+package xyz.android.sumardosnumeros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText num1, num2;
+    Button sumar;
+    TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        sumar = findViewById(R.id.sumar);
+        resultado = findViewById(R.id.resultado);
+
+        sumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int n1 = Integer.parseInt(num1.getText().toString());
+                int n2 = Integer.parseInt(num2.getText().toString());
+                int res = n1 + n2;
+                resultado.setText("El resultado es: " + res);
+            }
+        });
+
+    }
+}
